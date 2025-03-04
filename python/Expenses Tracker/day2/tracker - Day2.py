55f6370bd9891e2b5690ebac2e9176954fadc1d2
Justine Reinn Sanidad <namecode062@gmail.com>
2025-03-04 14:31:04 +0800

    Add files via upload

---
+++ python/Expenses Tracker/day2/tracker - Day2.py
@@ -0,0 +1,27 @@
+import json
+
+Expenses = []
+file_Path = "C:/Users/reinn/Desktop/projects/Expencestracker.json"
+print("========= Expenses Tracker=========")
+while True:
+    Choices = int(input("1. Add Expenses\n2. View Expenses\n3. Calculate Total Expenses\n4. Save & Exit\n"))
+    if Choices == 1:
+        category = input("Enter category: ")
+        amount = float(input("Enter amount: "))
+        Date = input("Enter date(yyyy-mm-dd): ")
+        print("Expense added successfully!")
+        Expenses.append({"category": category, "amount": amount, "Date": Date})
+        with open(file_Path, "a") as file:
+            json.dump(Expenses, file)
+        
+    elif Choices == 2:
+        for expense in Expenses:
+            print(f"Category: {expense['category']}, Amount: {expense['amount']}, Date: {expense['Date']}")
+    elif Choices == 3:
+        Total = sum(expense["amount"] for expense in Expenses)
+        print(f"Total Expenses:{Total}")
+    elif Choices == 4:
+        print("Saving and Exiting")
+        break
+    else:
+        print("Invalid output")
